769e2e127502ce1640d75ca03242c56a3c19c6b5
vinhlq <vinhlq2512@gmail.com>
2026-06-26 08:09:27 +0700

    feat: config docker data-root to /home/data/docker

--- docker-install.sh
+++ docker-install.sh
@@ -4,6 +4,11 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update -y
 sudo apt install docker-ce -y
+
+# Configure Docker to store all images and volumes in /home/data/docker
+sudo mkdir -p /etc/docker
+echo '{"data-root": "/home/data/docker"}' | sudo tee /etc/docker/daemon.json > /dev/null
+
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
